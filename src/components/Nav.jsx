508068b2d7989e398e5f03b0c6acdec8f8da3708
Branden Ng <brandendanielng@gmail.com>
2023-08-17 15:07:22 +1000

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,74 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../assets/images/logo.svg";
+import HamburgerIcon from "../assets/images/icons/hamburger.svg";
+import CancelIcon from "../assets/images/icons/cancel.svg";
+import "../assets/sass/components/nav.scss";
+import { barlow } from "../ultils/fonts";
+import { fraunces } from "../ultils/fonts";
+import { useState } from "react";
+
+const Nav = () => {
+  const [isMobileNavActive, setIsMobileNavActive] = useState(false);
+
+  const navToggle = () => {
+    setIsMobileNavActive(!isMobileNavActive);
+  };
+
+  return (
+    <nav className="nav">
+      <Image
+        src={Logo}
+        style={{ maxWidth: "163px", height: "100%", width: "100%" }}
+        className="nav__logo-brand"
+        alt="logo"
+      />
+
+      <div className="nav__default">
+        <div className={`nav__default-links ${barlow.className}`}>
+          <ul>
+            <li>
+              <Link href="/">HOME</Link>
+            </li>
+            <li>
+              <Link href="about-us">ABOUT US</Link>
+            </li>
+            <li>
+              <Link href="create-your-plan">CREATE YOUR PLAN</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="nav__mobile">
+        <button className="nav__mobile-toggle" onClick={navToggle}>
+          <Image src={isMobileNavActive ? CancelIcon : HamburgerIcon} />
+        </button>
+
+        <div
+          className={`nav__mobile-sidebar ${
+            isMobileNavActive ? "nav__mobile-sidebar--active" : ""
+          }`}
+        >
+          <div className={`nav__mobile-sidebar-links ${fraunces.className}`}>
+            <ul>
+              <li>
+                <Link href="/">HOME</Link>
+              </li>
+              <li>
+                <Link href="about-us">ABOUT US</Link>
+              </li>
+              <li>
+                <Link href="create-your-plan">CREATE YOUR PLAN</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
---
+++ src/ultils/fonts.js
@@ -0,0 +1,10 @@
+import { Fraunces, Barlow } from "next/font/google";
+
+export const fraunces = Fraunces({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+export const barlow = Barlow({
+  weight: ["400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
